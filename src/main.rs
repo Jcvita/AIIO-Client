3b9a5fa25e9c46f260a2296d2d5d0693599f1eb4
Joseph <jcvita2012@gmail.com>
2022-09-28 22:13:39 -0400

    remove unused example code

--- src/main.rs
+++ src/main.rs
@@ -25,25 +25,3 @@ fn main() {
         .body(&result)
         .show();
 }
-
-//Caesar cipher example code 
-//caesar cipher
-// println!("Enter the text to be encrypted");
-// let mut text = String::new();
-// io::stdin().read_line(&mut text).expect("Failed to read line");
-// let text = text.trim();
-// println!("Enter the key");
-// let mut key = String::new();
-// io::stdin().read_line(&mut key).expect("Failed to read line");
-// let key: u8 = key.trim().parse().expect("Please type a number!");
-// let mut encrypted_text = String::new();
-// for c in text.chars() {
-//     let mut c = c as u8;
-//     if c >= 65 && c <= 90 {
-//         c = ((c - 65 + key) % 26) + 65;
-//     } else if c >= 97 && c <= 122 {
-//         c = ((c - 97 + key) % 26) + 97;
-//     }
-//     encrypted_text.push(c as char);
-// }
-// println!("Encrypted text: {}", encrypted_text);
